3d52dcea9cd881a815eb5033b6a6ddf858d58f95
Aaron Barnes <aaron@io.net.nz>
2014-11-04 19:56:24 +1300

    person.py: Make person sprite "turn" when walking

--- person.py
+++ person.py
@@ -1,12 +1,25 @@
 import pygame
 
+DOWN    = 'd'
+UP      = 'u'
+LEFT    = 'l'
+RIGHT   = 'r'
+
 class person(pygame.sprite.Sprite):
 
-    src_image = None
-
+    size = 32
     increment = 32
+    sprites = {
+        DOWN:   (0,32),
+        UP:     (0,0),
+        LEFT:   (0,64),
+        RIGHT:  (0,96)
+    }
+
+    src_image = None
     x = 0
     y = 0
+    direction = DOWN
 
     action_queue = []
 
@@ -15,13 +28,12 @@ class person(pygame.sprite.Sprite):
     def __init__(self, image, position):
         pygame.sprite.Sprite.__init__(self)
 
-        image = pygame.image.load(image)
-        size = image.get_rect().size
-        self.src_image = pygame.transform.chop(image, (0, 0, size[0]-self.increment, size[1]-self.increment))
+        self.src_image = pygame.image.load(image)
 
         self.x = position[0]
         self.y = position[1]
-   
+        self.direction = DOWN
+
         self.action_queue = []
 
         self.renderer = pygame.sprite.RenderPlain(self)
@@ -31,15 +43,19 @@ class person(pygame.sprite.Sprite):
 
     def move_up(self):
         self.y -= self.increment
+        self.direction = UP
 
     def move_down(self):
         self.y += self.increment
+        self.direction = DOWN
 
     def move_left(self):
         self.x -= self.increment
-    
+        self.direction = LEFT
+
     def move_right(self):
         self.x += self.increment
+        self.direction = RIGHT
 
     def update(self, deltat, screen):
 
@@ -48,7 +64,11 @@ class person(pygame.sprite.Sprite):
             action = self.action_queue.pop()
             action()
 
-        self.image = self.src_image
+        self.image = pygame.Surface((self.size, self.size), pygame.SRCALPHA, 32)
+        self.image.convert_alpha()
+        self.image.blit(self.src_image, (0, 0), (self.sprites[self.direction][0], self.sprites[self.direction][1], self.size, self.size))
+
         self.rect = self.image.get_rect()
         self.rect.center = (self.x, self.y)
-        self.renderer.draw(screen)
+
+        screen.blit(self.image, self.rect)
